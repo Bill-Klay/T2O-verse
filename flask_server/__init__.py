361aae82c2e638ddca306e95839951404caefb25
Bill-Klay <bilal.khan2998@gmail.com>
2024-06-06 17:11:39 +0500

    add logging architecture

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -1,13 +1,18 @@
-from flask import Flask
+import logging
+from flask import Flask, has_request_context, request
+from flask.logging import default_handler
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_login import LoginManager
+from.config import get_logging_config
+from logging.handlers import SMTPHandler
 
 db = SQLAlchemy()
 mail = Mail()
 login_manager = LoginManager()
+logger = logging.getLogger(__name__)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -21,11 +26,20 @@ def create_app():
     # Load configurations
     app.config.from_pyfile('config.py')
     CORS(app, resources={r'*': {'origins': 'http://localhost:3000'}})
-
+    logging_config = get_logging_config(app.config['ENV'])
+    logging.basicConfig(
+        level=getattr(logging, logging_config['level'].upper()),
+        filename=logging_config['file'],
+        format=logging_config['format'],
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filemode=logging_config['filemode'],
+        force=True  # Reconfigure logging if called multiple times
+    )
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)  # Initialize Flask-Mail
     migrate = Migrate(app, db)
+
     # Register blueprints here
     with app.app_context():
         # Import parts of our application
--- flask_server/config.py
+++ flask_server/config.py
@@ -1,9 +1,35 @@
 import os
 
+def get_logging_config(env):
+    configs = {
+        'development': {
+            'filemode': 'a',
+            'level': 'DEBUG',
+            'file': 'dev.log',
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+        'production': {
+            'filemode': 'a',
+            'level': 'INFO',
+            'file': 'prod.log',
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+        'testing': {
+            'filemode': 'a',
+            'level': 'WARNING',
+            'file': 'test.log',
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        }
+    }
+
+    return configs.get(env, configs['development'])  # Default to 'development' if env is not found
+
+
 DEBUG = True
 SECRET_KEY = 'your_secret_key'
 # Add more configurations as needed
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///default.db')
+ENV = os.getenv('FLASK_ENV', 'development').lower()
 MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
 MAIL_PORT = 2525
 MAIL_USERNAME = 'acf7606c86c045'
