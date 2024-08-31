bbb0a1d378d00d67f0718975b0f87d24057a0cdd
Bill-Klay <bilal.khan2998@gmail.com>
2024-08-31 21:48:50 +0500

    update windows deploy batch script

--- deploy.bat
+++ deploy.bat
@@ -1,23 +1,45 @@
 @echo off
 
-SET FLASK_APP=../app_run.py
-SET FLASK_ENV=development
-SET FLASK_DEBUG=1
-SET DATABASE_URL=sqlite:///default.db
+REM Setting up environment variables
+REM Add more configurations as needed
+set FLASK_APP=app_run.py
+set FLASK_ENV=development
+set FLASK_DEBUG=1
+set DATABASE_URL=sqlite:///default.db
 
 echo Environment variables set.
 
+REM Database operations
 flask db init
-flask db migrate
-flask db upgrade
+if %ERRORLEVEL% NEQ 0 (
+    echo Database initialization failed
+    exit /b 1
+)
 
+flask db migrate -m "initial migration"
 if %ERRORLEVEL% NEQ 0 (
     echo Database migration failed
     exit /b 1
 )
 
-echo Database initialized and migrated.
+flask db upgrade
+if %ERRORLEVEL% NEQ 0 (
+    echo Database upgrade failed
+    exit /b 1
+) else (
+    echo Database initialized and migrated.
+)
 
+REM Run Python script to execute initialization
+python create_initial_data.py
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to create initial users
+    exit /b 1
+) else (
+    echo Initial users created successfully.
+)
+
+REM Starting the Flask application
 flask run
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to start Flask application
